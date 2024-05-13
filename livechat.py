6e409d249282e9914717cb67e9584a83e856f2ce
RGBBARROS <107211438+RGBBARROS@users.noreply.github.com>
2024-05-12 22:15:00 -0300

    Atualização com LiveChat pronto

--- livechat.py
+++ livechat.py
@@ -1,32 +1,94 @@
-# CRIAÇÃO DE UM LIVE CHAT
+# PASSO A PASSO
+# lIVECHAT
+# botao de iniciar chat
+# DIV para entrar no chat
+# Quando entrar no chat: (aparece para todo mundo)
+    # a mensagem que você entrou no chat
+    # o campo e o botão de enviar mensagem
+# a cada mensagem que você envia (aparece para todo mundo)
+    # Nome: Texto da Mensagem
 
 # INSTALAR FLET NO CMD -> pip install flet
-
-# IMPORTAR O FLET
+# IMPORTAR FLET AS FT
 import flet as ft
 
-# CRIAR A FUNÇÃO PRINCIPAL DO SISTEMA
-
-# Sempre criar o elemento e posteriormente agregar o elemento a página
-# Não esquecer de colocar o parametro do que voce quer que o elemento faça em sua página
 def main(pagina):
-    titulo = ft.Text("LiveChat")
+    texto = ft.Text("LiveChat")
 
-    
-def iniciar_chat(evento):
-    botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click= iniciar_chat)
+    chat = ft.Column()
 
-# ELEMENTOS AGREGADOS
+    nome_usuario = ft.TextField(label="Escreva seu cargo.seu nome")
 
-# A ordem com que vamos colocar os elementos faz diferença
-# pagina.add serve para colocar cada elemento na pagina, ou seja, é um pagina.add para cada elemento
-    pagina.add(titulo)
+    def enviar_mensagem_tunel(mensagem):
+        tipo = mensagem["tipo"]
+        if tipo == "mensagem":
+            texto_mensagem = mensagem["texto"]
+            usuario_mensagem = mensagem["usuario"]
+
+# ADICIONAR MSG AO CHAT
+            chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}"))
+        else:
+            usuario_mensagem = mensagem["usuario"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no chat", 
+                                         size=12, italic=True, color=ft.colors.ORANGE_500))
+        pagina.update()
+
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+
+    def enviar_mensagem(evento):
+        pagina.pubsub.send_all({"texto": campo_mensagem.value, "usuario": nome_usuario.value,
+                                "tipo": "mensagem"})
+        
+
+# LIMPAR O CAMPO MSG
+        campo_mensagem.value = ""
+        pagina.update()
+
+    campo_mensagem = ft.TextField(label="Digite uma mensagem", on_submit=enviar_mensagem)
+    botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+
+    def entrar_popup(evento):
+        pagina.pubsub.send_all({"usuario": nome_usuario.value, "tipo": "entrada"})
+# adicionar o chat
+        pagina.add(chat)
+# fechar o popup
+        popup.open = False
+# remover o botao iniciar chat
+        pagina.remove(botao_iniciar)
+        pagina.remove(texto)
+# criar o campo de mensagem do usuario
+# criar o botao de enviar mensagem do usuario
+        pagina.add(ft.Row(
+            [campo_mensagem, botao_enviar_mensagem]
+        ))
+        pagina.update()
+
+    popup = ft.AlertDialog(
+        open=False, 
+        modal=True,
+        title=ft.Text("Bem vindo ao Hashzap"),
+        content=nome_usuario,
+        actions=[ft.ElevatedButton("Entrar", on_click=entrar_popup)],
+        )
+
+    def entrar_chat(evento):
+        pagina.dialog = popup
+        popup.open = True
+        pagina.update()
+
+    botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=entrar_chat)
+
+    pagina.add(texto)
     pagina.add(botao_iniciar)
+
+ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
+
 
 # RODAR A SISTEMA
 
 # Caso queira rodar a aplicação de uma forma diferente de uma janela por ex. 
 # Pode-se colocar um paramentro dentro do (main) que é o "view", ou seja,
 # o código ficará assim: (main, view = ...)
-# Caso voce queira visualizar na web ficaria assim: (main, view = ft.web_browser)
-ft.app(main)
+# Caso voce queira visualizar na web ficaria assim: ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
+# Ou, ft.app(main) = para rodar como uma aplicação
+# REALIZAR DEPLOY
